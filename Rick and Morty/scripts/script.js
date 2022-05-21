daaa93512f1ad6f7b0ecb971fec385791bf6402a
Damian <tricasaurio@gmail.com>
2022-05-22 01:35:56 +0200

    Rick & Morty Fixed

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -5,7 +5,7 @@ let characterCardsElement;
 function renderCharacter(name, status, species, image) {
   const cardCharacterElement = document.createElement("div");
   cardCharacterElement.className = "character-card";
-  characterCardsElement.appendChild(cardCharacterElement);
+  mainAreaElement.appendChild(cardCharacterElement);
 
   const characterImageElement = document.createElement("img");
   characterImageElement.src = image;
@@ -13,84 +13,138 @@ function renderCharacter(name, status, species, image) {
   const characterNameElement = document.createElement("div");
   characterNameElement.innerText = name;
 
-  const characterSpeciesStatusElement = document.createElement("div");
-  characterSpeciesStatusElement.innerText = species + "|" + status;
+  const characterSpecieStatusElement = document.createElement("div");
+  characterSpecieStatusElement.innerText = `${species} | ${status}`;
 
   cardCharacterElement.appendChild(characterImageElement);
   cardCharacterElement.appendChild(characterNameElement);
-  cardCharacterElement.appendChild(characterSpeciesStatusElement);
+  cardCharacterElement.appendChild(characterSpecieStatusElement);
 }
 
-async function fetchCharacters(charactersURLs) {
-  const characterFetchPromises = charactersURLs.map((charactersURL) =>
-    fetch(charactersURL)
+async function fetchCharacters(characterURLs) {
+  const characterFetchPromises = characterURLs.map((characterURL) =>
+    fetch(characterURL)
   );
   const resolvedFetchResponses = await Promise.all(characterFetchPromises);
 
   const jsonPromises = resolvedFetchResponses.map((resolvedFetchResponse) =>
     resolvedFetchResponse.json()
   );
-  const resolvedjsonPromises = await Promise.all(jsonPromises);
+  const resolvedJsonPromises = await Promise.all(jsonPromises);
 
-  console.log(resolvedjsonPromises);
-
-  resolvedjsonPromises.forEach((characterJSON) =>
+  resolvedJsonPromises.forEach((characterJson) =>
     renderCharacter(
-      characterJSON.name,
-      characterJSON.status,
-      characterJSON.species,
-      characterJSON.image
+      characterJson.name,
+      characterJson.status,
+      characterJson.species,
+      characterJson.image
     )
   );
 }
 
-function updateMainArea(name, date, episodeCode, charactersURLs) {
+function updateMainArea(name, date, episodeCode, characterURLs) {
   mainAreaElement.innerHTML = "";
-  //characterCardsElement.innerHTML = "";
+  characterCardsElement.innerHTML = "";
 
   const titleElement = document.createElement("h2");
   titleElement.innerText = name;
   const dateAndCodeElement = document.createElement("h3");
-  dateAndCodeElement.innerText = date + "|" + episodeCode;
+  dateAndCodeElement.innerText = `${date} | ${episodeCode}`;
 
   mainAreaElement.appendChild(titleElement);
   mainAreaElement.appendChild(dateAndCodeElement);
+  mainAreaElement.appendChild(characterCardsElement);
 
-  renderCharacters(charactersURLs);
+  fetchCharacters(characterURLs);
+}
+
+function renderNextEpisodesButton(nextUrl) {
+  if (!nextUrl) {
+    return;
+  }
+  const nextButton = document.createElement("button");
+  nextButton.className = "next-list-button";
+  nextButton.innerText = "Next episodes ▶️";
+  nextButton.addEventListener("click", (_event) => {
+    console.log(nextUrl);
+    fetch(nextUrl)
+      .then((response) => response.json())
+      .then((json) => {
+        renderListOfEpisodes(json.results, json.info.next);
+      });
+  });
+  sidebarElement.appendChild(nextButton);
+}
+
+function renderListOfEpisodes(episodes, nextUrl) {
+  document
+    .querySelectorAll(".next-list-button")
+    .forEach((buttonElement) => sidebarElement.removeChild(buttonElement));
+
+  episodes.forEach((episode) => {
+    const titleElement = document.createElement("p");
+    titleElement.innerText = `Episode ${episode.id}`;
+    sidebarElement.appendChild(titleElement);
+    titleElement.addEventListener("click", (_event) => {
+      updateMainArea(
+        episode.name,
+        episode.air_date,
+        episode.episode,
+        episode.characters
+      );
+    });
+  });
+  renderNextEpisodesButton(nextUrl);
 }
 
 function sidebar() {
-  const sidebarElement = document.createElement("div");
+  sidebarElement = document.createElement("div");
   sidebarElement.id = "sidebar";
-
-  document.getElementById("root").appendChild(sidebarElement);
-
+  document.querySelector("#root").appendChild(sidebarElement);
   fetch("https://rickandmortyapi.com/api/episode")
-    .then((result) => result.json())
+    .then((response) => response.json())
     .then((json) => {
-      console.log(json.results);
-      json.results.forEach((episode) => {
-        const titleElement = document.createElement("p");
-        titleElement.innerText = "Episode" + episode.id;
-        sidebarElement.appendChild(titleElement);
-        titleElement.addEventListener("click", (_event) => {
-          updateMainArea(
-            episode.name,
-            episode.air_date,
-            episode.episode,
-            episode.characters
-          );
-        });
-        console.log("Episode " + episode);
-      });
-    });
+      renderListOfEpisodes(json.results, json.info.next);
+      const firstEpisode = json.results[0];
+      updateMainArea(
+        firstEpisode.name,
+        firstEpisode.air_date,
+        firstEpisode.episode,
+        firstEpisode.characters
+      );
+    })
+    .catch((errorMessage) => console.error(errorMessage));
 }
 
 function mainArea() {
-  const mainAreaElement = document.createElement("div");
-  document.getElementById("root").appendChild(mainAreaElement);
-  mainAreaElement.innerText = "";
+  mainAreaElement = document.createElement("div");
+  mainAreaElement.id = "main-area";
+  characterCardsElement = document.createElement("div");
+  characterCardsElement.id = "character-cards";
+  mainAreaElement.appendChild(characterCardsElement);
+  document.querySelector("#root").appendChild(mainAreaElement);
 }
 
 sidebar();
 mainArea();
+
+const promiseFetch = new Promise((resolve, reject) => {
+  const response = 200;
+  if (response !== 200) {
+    reject(`Something went wrong, status code is not 200, it is: ${response}`);
+  }
+  resolve((response, 100));
+});
+
+// Structure of HOW to work with promises
+// promiseFetch(
+//     .then(
+//         (response, code) => console.log(response, code) // 200, 100
+//     )
+//     .catch(
+//         errorMessage => console.error(errorMessage) // ()
+//     )
+//     .finally(
+//         () => console.log(`Finally, I'm done explaining promises`)
+//     );
+// )
