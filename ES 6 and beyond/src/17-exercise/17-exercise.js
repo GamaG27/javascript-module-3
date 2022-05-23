3d0525ca0d4d3c62924b3c009dbde32ec7ba699b
Damian <tricasaurio@gmail.com>
2022-05-23 10:16:28 +0200

    Finishing homework

--- ES 6 and beyond/src/17-exercise/17-exercise.js
+++ ES 6 and beyond/src/17-exercise/17-exercise.js
@@ -21,7 +21,14 @@
  */
 
 // Finish the code of the function
-function exercise17() {}
+function exercise17() {
+    const jsonplaceholder = fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+    )
+        .then((res) => res.json())
+        .then((body) => displayContents(body));
+    return jsonplaceholder;
+}
 
 // Don’t change the code bellow this line
 function displayContents(data) {
